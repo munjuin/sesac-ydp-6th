5791417873f15de05faaf01f8f9626a589e9c0fa
munjuin <raiawa1212@gmail.com>
2024-06-17 16:16:40 +0900

    study: AJAX

---
+++ lecture/12-form-dynamic/index.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = 8080;
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+    res.render('dynamic', { title: '동적 폼을 배워보자!' });
+})
+app.get('/ajax', (req, res)=>{
+  console.log(req.query);
+  res.send(req.query);
+})
+app.post('/ajax', (req, res)=>{
+  console.log(req.body);
+  res.send(req.body);
+})
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`);
+})
