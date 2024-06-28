806a2845234d2d05410dc99db9033d26e53d8cba
munjuin <raiawa1212@gmail.com>
2024-06-28 19:05:05 +0900

    study: sequelize 1:1 1:n n:n

--- lecture/17-sequelize/models/Team.js
+++ lecture/17-sequelize/models/Team.js
@@ -1,20 +1,20 @@
 const TeamModel = (sequelize, DataTypes) => {
-  const Team = sequelize.define('Team', {
-    team_id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      allowNull: false,
-      autoIncrement: true,
-    },
-    name: {
-      type: DataTypes.STRING(63),
-      allowNull: false,
-    },
-  }, {
-    freezeTableName: true
-  });
-  
-  return Team;
+    const Team = sequelize.define('Team', {
+      team_id: {
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        allowNull: false,
+        autoIncrement: true,
+      },
+      name: {
+        type: DataTypes.STRING(63),
+        allowNull: false,
+      },
+    }, {
+      freezeTableName: true
+    });
+    
+    return Team;
 };
-
+  
 module.exports = TeamModel;
