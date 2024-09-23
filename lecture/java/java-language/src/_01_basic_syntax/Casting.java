7dfc13db2c7bf7ad34cf6de605b2dfec1ed08558
munjuin <raiawa1212@gmail.com>
2024-09-23 16:31:38 +0900

    study: java basic_syntax

---
+++ lecture/java/java-language/src/_01_basic_syntax/Casting.java
@@ -0,0 +1,34 @@
+package _01_basic_syntax;
+
+//형변환
+//데이터 타입을 변환하는 것
+//(type)var
+//type: 변경하려는 데이터 타입
+//var: 변수
+public class Casting {
+    public static void main(String[] args) {
+        //묵시적 형변환 wider conversion
+        //더 작은 타입에서 더 큰 타입으로의 자동 형변환
+        //반대의 경우에는 묵시적 형변환이 불가능 -> 명시적으로 변환해야 함
+        int smallNumber = 10;
+        System.out.println("smallNumber = " + smallNumber);
+        double bigNumber = smallNumber;//int를 doubale로 자동 형변환
+        System.out.println("bigNuber = " + bigNumber);
+
+
+
+        //////////////////////////////////////////////////////////
+        //명시적 형변환 narrow conversion
+        //더 큰 타입에서 작은 타입으로 형변환
+        double anotherBigNumber = 20.5;
+        System.out.println("anotherBigNumber = " + anotherBigNumber);
+        int anotherSmallNumber = (int)anotherBigNumber;//int를 double로 형변환
+        System.out.println("anotherSmallNumber = " + anotherSmallNumber);
+
+        //주의. 명시적 형변환의 경우 데이터 손실이 발생할 수 있음
+        int largeNumber  = 1000;
+        System.out.println("largeNumber = " + largeNumber);
+        byte smallByte = (byte) largeNumber;
+        System.out.println("smallByte = " + smallByte);
+    }
+}
---
+++ lecture/java/java-language/src/_01_basic_syntax/InputOutputPractice.java
@@ -0,0 +1,18 @@
+package _01_basic_syntax;
+
+import java.util.Scanner;
+
+public class InputOutputPractice {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        System.out.println("이름을 입력하세요");
+        String name = scanner.next();
+        System.out.println("나이를 입력하세요");
+        int age = scanner.nextInt();
+
+        System.out.printf("안녕하세요! %s님 (%d)세", name, age);
+        scanner.close();
+    }
+}
